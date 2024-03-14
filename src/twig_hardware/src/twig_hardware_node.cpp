f81e58e67d0bba600610fc64cbecbeb65735b3f5
nosknut <nosknut@gmail.com>
2024-03-14 21:39:53 +0000

    Adds twig_hardware

---
+++ src/twig_hardware/src/twig_hardware_node.cpp
@@ -0,0 +1,406 @@
+#include "twig_hardware/twig_lib.hpp"
+
+#include <sensor_msgs/msg/joy.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <rclcpp/client.hpp>
+#include <rclcpp/experimental/buffers/intra_process_buffer.hpp>
+#include <rclcpp/node.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/qos.hpp>
+#include <rclcpp/qos_event.hpp>
+#include <rclcpp/subscription.hpp>
+#include <rclcpp/time.hpp>
+#include <rclcpp/utilities.hpp>
+#include <std_srvs/srv/trigger.hpp>
+
+const std::string NODE_NAME = "twig_hardware_node";
+
+const std::string PARAM_PUBLISH_RATE = "publish_rate";
+const std::string PARAM_PUSH_RATE = "push_rate";
+
+namespace twig_hardware
+{
+
+class TwigHardwareNode : public rclcpp::Node
+{
+protected:
+  TwigLib twig;
+  rclcpp::TimerBase::SharedPtr publish_timer_;
+  rclcpp::TimerBase::SharedPtr push_timer_;
+
+  long int publish_period_ = 1e6;
+  long int push_period_ = 1e6;
+
+  bool has_new_command_ = false;
+
+  // Activate Services
+
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr shoulder_servo_activate_srv_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr wrist_servo_activate_srv_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr gripper_servo_activate_srv_;
+
+  // Deactivate Services
+
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr shoulder_servo_deactivate_srv_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr wrist_servo_deactivate_srv_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr gripper_servo_deactivate_srv_;
+
+  // velocity Subscribers
+
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr shoulder_servo_velocity_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr wrist_servo_velocity_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr gripper_servo_velocity_sub_;
+
+  // Activation Status Publishers
+
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr shoulder_servo_activation_status_pub_;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr wrist_servo_activation_status_pub_;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr gripper_servo_activation_status_pub_;
+
+  // Current Publishers
+
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr shoulder_servo_current_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wrist_servo_current_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr gripper_servo_current_pub_;
+
+  // Voltage Publishers
+
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr shoulder_servo_voltage_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wrist_servo_voltage_pub_;
+
+  // Velocity Publishers
+
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr shoulder_servo_velocity_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wrist_servo_velocity_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr gripper_servo_velocity_pub_;
+
+  // Position Publishers
+
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr shoulder_servo_position_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wrist_servo_position_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr gripper_servo_position_pub_;
+
+  // Effort Publishers
+
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr shoulder_servo_effort_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr wrist_servo_effort_pub_;
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr gripper_servo_effort_pub_;
+
+  // Helpers
+
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr create_float_publisher(
+    const std::string & topic_name)
+  {
+    return this->create_publisher<std_msgs::msg::Float32>(
+      topic_name,
+      rclcpp::SystemDefaultsQoS()
+    );
+  }
+
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr create_bool_publisher(
+    const std::string & topic_name)
+  {
+    return this->create_publisher<std_msgs::msg::Bool>(
+      topic_name,
+      rclcpp::SystemDefaultsQoS()
+    );
+  }
+
+  void publish_float(const rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr & pub, double value)
+  {
+    pub->publish(std_msgs::msg::Float32().set__data(value));
+  }
+
+  void publish_state()
+  {
+    // Activation Status Publishers
+    shoulder_servo_activation_status_pub_->publish(
+      std_msgs::msg::Bool().set__data(twig.get_shoulder_servo_activation_status()));
+
+    wrist_servo_activation_status_pub_->publish(
+      std_msgs::msg::Bool().set__data(twig.get_wrist_servo_activation_status()));
+
+    gripper_servo_activation_status_pub_->publish(
+      std_msgs::msg::Bool().set__data(twig.get_gripper_servo_activation_status()));
+
+    // Current Publishers
+    publish_float(shoulder_servo_current_pub_, twig.get_shoulder_servo_current());
+    publish_float(wrist_servo_current_pub_, twig.get_wrist_servo_current());
+    publish_float(gripper_servo_current_pub_, twig.get_gripper_servo_current());
+
+    // Voltage Publishers
+    publish_float(shoulder_servo_voltage_pub_, twig.get_shoulder_servo_voltage());
+    publish_float(wrist_servo_voltage_pub_, twig.get_wrist_servo_voltage());
+
+    // Velocity Publishers
+    publish_float(shoulder_servo_velocity_pub_, twig.get_shoulder_servo_velocity());
+    publish_float(wrist_servo_velocity_pub_, twig.get_wrist_servo_velocity());
+    publish_float(gripper_servo_velocity_pub_, twig.get_gripper_servo_velocity());
+
+    // Position Publishers
+    publish_float(shoulder_servo_position_pub_, twig.get_shoulder_servo_position());
+    publish_float(wrist_servo_position_pub_, twig.get_wrist_servo_position());
+    publish_float(gripper_servo_position_pub_, twig.get_gripper_servo_position());
+
+    // Effort Publishers
+    publish_float(shoulder_servo_effort_pub_, twig.get_shoulder_servo_effort());
+    publish_float(wrist_servo_effort_pub_, twig.get_wrist_servo_effort());
+    publish_float(gripper_servo_effort_pub_, twig.get_gripper_servo_effort());
+  }
+
+  int get_total_subscriber_count()
+  {
+    int count = 0;
+
+    // Activation Status Publishers
+    count += shoulder_servo_activation_status_pub_->get_subscription_count();
+    count += wrist_servo_activation_status_pub_->get_subscription_count();
+    count += gripper_servo_activation_status_pub_->get_subscription_count();
+
+    // Current Publishers
+    count += shoulder_servo_current_pub_->get_subscription_count();
+    count += wrist_servo_current_pub_->get_subscription_count();
+    count += gripper_servo_current_pub_->get_subscription_count();
+
+    // Voltage Publishers
+    count += shoulder_servo_voltage_pub_->get_subscription_count();
+    count += wrist_servo_voltage_pub_->get_subscription_count();
+
+    // Velocity Publishers
+    count += shoulder_servo_velocity_pub_->get_subscription_count();
+    count += wrist_servo_velocity_pub_->get_subscription_count();
+    count += gripper_servo_velocity_pub_->get_subscription_count();
+
+    // Position Publishers
+    count += shoulder_servo_position_pub_->get_subscription_count();
+    count += wrist_servo_position_pub_->get_subscription_count();
+    count += gripper_servo_position_pub_->get_subscription_count();
+
+    // Effort Publishers
+    count += shoulder_servo_effort_pub_->get_subscription_count();
+    count += wrist_servo_effort_pub_->get_subscription_count();
+    count += gripper_servo_effort_pub_->get_subscription_count();
+
+    return count;
+  }
+
+  void publish_timer_callback()
+  {
+    if (get_total_subscriber_count() > 0) {
+      if (twig.read_state()) {
+        twig.update_velocities(publish_period_);
+        publish_state();
+      } else {
+        RCLCPP_ERROR_ONCE(this->get_logger(), "Failed to read state from hardware!");
+      }
+    }
+  }
+
+  void push_timer_callback()
+  {
+    if (has_new_command_) {
+      if (!twig.write_command()) {
+        RCLCPP_ERROR_ONCE(this->get_logger(), "Failed to write command to hardware!");
+      }
+    }
+  }
+
+public:
+  TwigHardwareNode(const rclcpp::NodeOptions & options)
+  : Node(NODE_NAME, options)
+  {
+
+    this->declare_parameter(PARAM_PUBLISH_RATE, 50);
+    this->declare_parameter(PARAM_PUSH_RATE, 50);
+
+    // Activate Services
+    // Black magic that allows running services with class instance methods is based on:
+    // https://github.com/ros2/demos/blob/humble/demo_nodes_cpp/src/services/add_two_ints_server.cpp#L36-L48
+    shoulder_servo_activate_srv_ = create_service<std_srvs::srv::Trigger>(
+      "shoulder/servo/activate",
+      [this](
+        const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+        std::shared_ptr<std_srvs::srv::Trigger::Response> response
+      ) -> void {
+        twig.activate_shoulder_servo();
+
+        if (twig.write_command()) {
+          response->success = true;
+          response->message = "Successfully activated shoulder servo";
+        } else {
+          response->success = false;
+          response->message = "Failed to write command to hardware";
+        }
+      });
+
+    wrist_servo_activate_srv_ = this->create_service<std_srvs::srv::Trigger>(
+      "wrist/servo/activate",
+      [this](
+        const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+        std::shared_ptr<std_srvs::srv::Trigger::Response> response
+      ) -> void {
+        twig.activate_wrist_servo();
+
+        if (twig.write_command()) {
+          response->success = true;
+          response->message = "Successfully activated wrist servo";
+        } else {
+          response->success = false;
+          response->message = "Failed to write command to hardware";
+        }
+      });
+
+    gripper_servo_activate_srv_ = this->create_service<std_srvs::srv::Trigger>(
+      "gripper/servo/activate",
+      [this](
+        const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+        std::shared_ptr<std_srvs::srv::Trigger::Response> response
+      ) -> void {
+        twig.activate_gripper_servo();
+
+        if (twig.write_command()) {
+          response->success = true;
+          response->message = "Successfully activated gripper servo";
+        } else {
+          response->success = false;
+          response->message = "Failed to write command to hardware";
+        }
+      });
+
+    // Deactivate Services
+    shoulder_servo_deactivate_srv_ = this->create_service<std_srvs::srv::Trigger>(
+      "shoulder/servo/deactivate",
+      [this](
+        const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+        std::shared_ptr<std_srvs::srv::Trigger::Response> response
+      ) -> void {
+        twig.deactivate_shoulder_servo();
+
+        if (twig.write_command()) {
+          response->success = true;
+          response->message = "Successfully deactivated shoulder servo";
+        } else {
+          response->success = false;
+          response->message = "Failed to write command to hardware";
+        }
+      });
+
+    wrist_servo_deactivate_srv_ = this->create_service<std_srvs::srv::Trigger>(
+      "wrist/servo/deactivate",
+      [this](
+        const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+        std::shared_ptr<std_srvs::srv::Trigger::Response> response
+      ) -> void {
+        twig.deactivate_wrist_servo();
+
+        if (twig.write_command()) {
+          response->success = true;
+          response->message = "Successfully deactivated wrist servo";
+        } else {
+          response->success = false;
+          response->message = "Failed to write command to hardware";
+        }
+      });
+
+    gripper_servo_deactivate_srv_ = this->create_service<std_srvs::srv::Trigger>(
+      "gripper/servo/deactivate",
+      [this](
+        const std::shared_ptr<rmw_request_id_t> request_header,
+        const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+        std::shared_ptr<std_srvs::srv::Trigger::Response> response
+      ) -> void {
+        twig.deactivate_gripper_servo();
+
+        if (twig.write_command()) {
+          response->success = true;
+          response->message = "Successfully deactivated gripper servo";
+        } else {
+          response->success = false;
+          response->message = "Failed to write command to hardware";
+        }
+      });
+
+    // Velocity Subscribers
+    shoulder_servo_velocity_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+      "shoulder/servo/velocity/cmd",
+      rclcpp::SystemDefaultsQoS(),
+      [this](const std_msgs::msg::Float32::SharedPtr msg) {
+        twig.set_shoulder_servo_velocity(msg->data);
+        has_new_command_ = true;
+      }
+    );
+
+    wrist_servo_velocity_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+      "wrist/servo/velocity/cmd",
+      rclcpp::SystemDefaultsQoS(),
+      [this](const std_msgs::msg::Float32::SharedPtr msg) {
+        twig.set_wrist_servo_velocity(msg->data);
+        has_new_command_ = true;
+      }
+    );
+
+    gripper_servo_velocity_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+      "gripper/servo/velocity/cmd",
+      rclcpp::SystemDefaultsQoS(),
+      [this](const std_msgs::msg::Float32::SharedPtr msg) {
+        twig.set_gripper_servo_velocity(msg->data);
+        has_new_command_ = true;
+      }
+    );
+
+    // Activation Status Publishers
+    shoulder_servo_activation_status_pub_ =
+      create_bool_publisher("shoulder/servo/status/activation");
+    wrist_servo_activation_status_pub_ = create_bool_publisher("wrist/servo/status/activation");
+    gripper_servo_activation_status_pub_ = create_bool_publisher("gripper/servo/status/activation");
+
+    // Current Publishers
+    shoulder_servo_current_pub_ = create_float_publisher("shoulder/servo/current");
+    wrist_servo_current_pub_ = create_float_publisher("wrist/servo/current");
+    gripper_servo_current_pub_ = create_float_publisher("gripper/servo/current");
+
+    // Voltage Publishers
+    shoulder_servo_voltage_pub_ = create_float_publisher("shoulder/servo/voltage");
+    wrist_servo_voltage_pub_ = create_float_publisher("wrist/servo/voltage");
+
+    // Velocity Publishers
+    shoulder_servo_velocity_pub_ = create_float_publisher("shoulder/servo/velocity");
+    wrist_servo_velocity_pub_ = create_float_publisher("wrist/servo/velocity");
+    gripper_servo_velocity_pub_ = create_float_publisher("gripper/servo/velocity");
+
+    // Position Publishers
+    shoulder_servo_position_pub_ = create_float_publisher("shoulder/servo/position");
+    wrist_servo_position_pub_ = create_float_publisher("wrist/servo/position");
+    gripper_servo_position_pub_ = create_float_publisher("gripper/servo/position");
+
+    // Effort Publishers
+    shoulder_servo_effort_pub_ = create_float_publisher("shoulder/servo/effort");
+    wrist_servo_effort_pub_ = create_float_publisher("wrist/servo/effort");
+    gripper_servo_effort_pub_ = create_float_publisher("gripper/servo/effort");
+
+    // Timers
+    publish_period_ = 1e6 / this->get_parameter(PARAM_PUBLISH_RATE).as_int();
+    publish_timer_ = this->create_wall_timer(
+      std::chrono::microseconds(publish_period_),
+      std::bind(&TwigHardwareNode::publish_timer_callback, this)
+    );
+
+    push_period_ = 1e6 / this->get_parameter(PARAM_PUSH_RATE).as_int();
+    push_timer_ = this->create_wall_timer(
+      std::chrono::microseconds(push_period_),
+      std::bind(&TwigHardwareNode::push_timer_callback, this)
+    );
+  }
+};
+}
+
+// Register the component with class_loader
+#include <rclcpp_components/register_node_macro.hpp>
+RCLCPP_COMPONENTS_REGISTER_NODE(twig_hardware::TwigHardwareNode)
---
+++ src/twig_hardware/src/twig_lib.cpp
@@ -0,0 +1,324 @@
+#include "twig_hardware/twig_lib.hpp"
+
+twig_hardware::TwigLib::TwigLib(int i2c_bus, int i2c_address, std::string i2c_device)
+: i2c_bus(i2c_bus), i2c_address(i2c_address), i2c_device(i2c_device)
+{
+}
+
+twig_hardware::TwigLib::~TwigLib()
+{
+  i2c_close_connection();
+}
+
+// I2c
+
+bool twig_hardware::TwigLib::i2c_open_connection()
+{
+  int connection = open(i2c_device.c_str(), O_RDWR);
+
+  if (connection < 0) {
+    std::cerr << "Error opening device\n";
+    return false;
+  }
+
+  if (ioctl(connection, I2C_SLAVE, i2c_address) < 0) {
+    std::cerr << "Error setting I2C address\n";
+    return false;
+  }
+
+  i2c_connection = connection;
+
+  return true;
+}
+
+bool twig_hardware::TwigLib::i2c_close_connection()
+{
+  if (i2c_connection != -1) {
+    close(i2c_connection);
+    i2c_connection = -1;
+    return true;
+  }
+  return false;
+}
+
+bool twig_hardware::TwigLib::i2c_send(std::byte * data, int data_size)
+{
+  i2c_open_connection();
+
+  // Send the I2C message
+  if (write(i2c_connection, data, data_size) != data_size) {
+    std::cerr << "Error sending data, ignoring message...\n";
+    i2c_close_connection();
+    return false;
+  }
+
+  i2c_close_connection();
+
+  return true;
+}
+
+bool twig_hardware::TwigLib::i2c_read(std::byte * buffer, int data_size)
+{
+  i2c_open_connection();
+
+  // Request the state from the i2c device
+  if (read(i2c_connection, buffer, data_size) != data_size) {
+    std::cerr << "Error reading data, ignoring message...\n";
+    i2c_close_connection();
+    return false;
+  }
+
+  i2c_close_connection();
+
+  return true;
+}
+
+// Sync
+
+bool twig_hardware::TwigLib::write_command()
+{
+  return i2c_send(reinterpret_cast<std::byte *>(&command), sizeof(command));
+}
+
+bool twig_hardware::TwigLib::read_state()
+{
+  return i2c_read(reinterpret_cast<std::byte *>(&state), sizeof(state));
+}
+
+bool twig_hardware::TwigLib::update_velocities(double delta_time)
+{
+  bool updated = false;
+
+  if (delta_time < velocity_timeout) {
+    shoulder_velocity = (get_shoulder_servo_position() - previous_shoulder_position) / delta_time;
+    wrist_velocity = (get_wrist_servo_position() - previous_wrist_position) / delta_time;
+    gripper_velocity = (get_gripper_servo_position() - previous_gripper_position) / delta_time;
+
+    updated = true;
+  }
+
+  previous_shoulder_position = get_shoulder_servo_position();
+  previous_wrist_position = get_wrist_servo_position();
+  previous_gripper_position = get_gripper_servo_position();
+
+  return updated;
+}
+
+// Utility
+
+double raw_to_radians(int16_t raw)
+{
+  return (((double) raw) / 4096.0) * 2 * M_PI;
+}
+
+// TODO: Implement voltage estimation
+double raw_to_voltage(int16_t raw)
+{
+  return ((double) raw) / 1024.0;
+}
+
+// TODO: Implement current estimation
+double raw_to_current(int16_t raw)
+{
+  return ((double) raw) / 1024.0;
+}
+
+// TODO: Implement effort estimation
+double twig_hardware::TwigLib::current_to_effort(double current)
+{
+  return current;
+}
+
+// Activate
+
+void twig_hardware::TwigLib::activate_shoulder_servo()
+{
+  stop_shoulder_servo();
+  command.shoulderServoPowered = true;
+}
+
+void twig_hardware::TwigLib::activate_wrist_servo()
+{
+  stop_wrist_servo();
+  command.wristServoPowered = true;
+}
+
+void twig_hardware::TwigLib::activate_gripper_servo()
+{
+  stop_gripper_servo();
+  command.gripperServoPowered = true;
+}
+
+void twig_hardware::TwigLib::activate_all_servos()
+{
+  activate_shoulder_servo();
+  activate_wrist_servo();
+  activate_gripper_servo();
+}
+
+// Deactivate
+
+void twig_hardware::TwigLib::deactivate_shoulder_servo()
+{
+  stop_shoulder_servo();
+  command.shoulderServoPowered = false;
+}
+
+void twig_hardware::TwigLib::deactivate_wrist_servo()
+{
+  stop_wrist_servo();
+  command.wristServoPowered = false;
+}
+
+void twig_hardware::TwigLib::deactivate_gripper_servo()
+{
+  stop_gripper_servo();
+  command.gripperServoPowered = false;
+}
+
+void twig_hardware::TwigLib::deactivate_all_servos()
+{
+  deactivate_shoulder_servo();
+  deactivate_wrist_servo();
+  deactivate_gripper_servo();
+}
+
+// Set velocity
+
+// TODO: Implement velocity control
+
+void twig_hardware::TwigLib::set_shoulder_servo_velocity(double velocity)
+{
+  command.shoulder = velocity;
+}
+
+void twig_hardware::TwigLib::set_wrist_servo_velocity(double velocity)
+{
+  command.wrist = velocity;
+}
+
+void twig_hardware::TwigLib::set_gripper_servo_velocity(double velocity)
+{
+  command.gripper = velocity;
+}
+
+// Stop
+
+void twig_hardware::TwigLib::stop_shoulder_servo()
+{
+  command.shoulder = 0;
+}
+
+void twig_hardware::TwigLib::stop_wrist_servo()
+{
+  command.wrist = 0;
+}
+
+void twig_hardware::TwigLib::stop_gripper_servo()
+{
+  command.gripper = 0;
+}
+
+void twig_hardware::TwigLib::stop_all_servos()
+{
+  stop_shoulder_servo();
+  stop_wrist_servo();
+  stop_gripper_servo();
+}
+
+// Get activation status
+
+double twig_hardware::TwigLib::get_shoulder_servo_activation_status()
+{
+  return state.shoulderServoPowered;
+}
+
+double twig_hardware::TwigLib::get_wrist_servo_activation_status()
+{
+  return state.wristServoPowered;
+}
+
+double twig_hardware::TwigLib::get_gripper_servo_activation_status()
+{
+  return state.gripperServoPowered;
+}
+
+// Get current
+
+double twig_hardware::TwigLib::get_shoulder_servo_current()
+{
+  return raw_to_current(state.shoulderCurrent);
+}
+
+double twig_hardware::TwigLib::get_wrist_servo_current()
+{
+  return raw_to_current(state.wristCurrent);
+}
+
+double twig_hardware::TwigLib::get_gripper_servo_current()
+{
+  return raw_to_current(state.gripperCurrent);
+}
+
+// Get voltage
+
+double twig_hardware::TwigLib::get_shoulder_servo_voltage()
+{
+  return raw_to_voltage(state.shoulderVoltage);
+}
+
+double twig_hardware::TwigLib::get_wrist_servo_voltage()
+{
+  return raw_to_voltage(state.wristVoltage);
+}
+
+// Get velocity
+
+double twig_hardware::TwigLib::get_shoulder_servo_velocity()
+{
+  return shoulder_velocity;
+}
+
+double twig_hardware::TwigLib::get_wrist_servo_velocity()
+{
+  return wrist_velocity;
+}
+
+double twig_hardware::TwigLib::get_gripper_servo_velocity()
+{
+  return gripper_velocity;
+}
+
+// Get position
+
+double twig_hardware::TwigLib::get_shoulder_servo_position()
+{
+  return raw_to_radians(state.shoulderPosition);
+}
+
+double twig_hardware::TwigLib::get_wrist_servo_position()
+{
+  return raw_to_radians(state.wristPosition);
+}
+
+double twig_hardware::TwigLib::get_gripper_servo_position()
+{
+  return raw_to_radians(state.gripperPosition);
+}
+
+// Get effort
+
+double twig_hardware::TwigLib::get_shoulder_servo_effort()
+{
+  return current_to_effort(get_shoulder_servo_current());
+}
+
+double twig_hardware::TwigLib::get_wrist_servo_effort()
+{
+  return current_to_effort(get_wrist_servo_current());
+}
+
+double twig_hardware::TwigLib::get_gripper_servo_effort()
+{
+  return current_to_effort(get_gripper_servo_current());
+}
