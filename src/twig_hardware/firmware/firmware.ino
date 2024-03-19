4ff2ab44e511aa5a0cbe85e87f884cba52bf05f7
Knut Ola Nøsen <nosknut@gmail.com>
2024-03-19 14:01:43 +0100

    Fixes struct padding

--- src/twig_hardware/firmware/firmware.ino
+++ src/twig_hardware/firmware/firmware.ino
@@ -52,7 +52,12 @@ struct TwigCommand
   bool gripperServoPowered = false;
 };
 
-struct TwigState
+// Attribute packed is used to ensure the struct has the same
+// shape across different architectures.
+// This allows it to be sent through the i2c bus.
+// https://stackoverflow.com/questions/21092415/force-c-structure-to-pack-tightly
+// https://www.geeksforgeeks.org/how-to-avoid-structure-padding-in-c/
+struct __attribute__((packed)) TwigState
 {
   int16_t wristPosition = 0;
   int16_t gripperPosition = 0;
