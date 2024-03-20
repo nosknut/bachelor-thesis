af5a656fc6f31a89158079a8de3275fa616e8bd5
Knut Ola Nøsen <nosknut@gmail.com>
2024-03-20 12:34:17 +0100

    Adds direct launchfile

---
+++ src/twig/launch/direct.launch.py
@@ -0,0 +1,23 @@
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def launch_file(package, launch_file):
+    return IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory(package), "launch", launch_file)]
+        )
+    )
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            launch_file("twig_teleop", "joy.launch.py"),
+            launch_file("twig_teleop", "foxglove.launch.py"),
+            launch_file("twig_teleop", "direct_teleop.launch.py"),
+            launch_file("twig_hardware", "node.launch.py"),
+        ]
+    )
