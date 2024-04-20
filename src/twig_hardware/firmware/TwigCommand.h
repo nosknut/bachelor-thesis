aeab1c99b52a94fc8de3ceb3682a0ee27a0a559f
Knut Ola Nøsen <nosknut@gmail.com>
2024-04-20 04:26:50 +0200

    Adds packed attribute to TwigCommand

--- src/twig_hardware/firmware/TwigCommand.h
+++ src/twig_hardware/firmware/TwigCommand.h
@@ -1,7 +1,12 @@
 #ifndef TWIG_COMMAND_h
 #define TWIG_COMMAND_h
 
-struct TwigCommand
+// Attribute packed is used to ensure the struct has the same
+// shape across different architectures.
+// This allows it to be sent through the i2c bus.
+// https://stackoverflow.com/questions/21092415/force-c-structure-to-pack-tightly
+// https://www.geeksforgeeks.org/how-to-avoid-structure-padding-in-c/
+struct __attribute__((packed)) TwigCommand
 {
   int16_t wrist = 0;
   int16_t gripper = 0;
--- src/twig_hardware/include/twig_hardware/twig_lib.hpp
+++ src/twig_hardware/include/twig_hardware/twig_lib.hpp
@@ -18,7 +18,12 @@
 
 namespace twig_hardware
 {
-struct TwigCommand
+// Attribute packed is used to ensure the struct has the same
+// shape across different architectures.
+// This allows it to be sent through the i2c bus.
+// https://stackoverflow.com/questions/21092415/force-c-structure-to-pack-tightly
+// https://www.geeksforgeeks.org/how-to-avoid-structure-padding-in-c/
+struct __attribute__((packed)) TwigCommand
 {
   int16_t wrist = 0;
   int16_t gripper = 0;
