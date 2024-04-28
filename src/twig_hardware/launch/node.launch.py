c1f2f805def4beef47d408f39734a055135f1831
Knut Ola Nøsen <nosknut@gmail.com>
2024-04-28 19:04:23 +0200

    Adds hardware node config

--- src/twig_hardware/launch/node.launch.py
+++ src/twig_hardware/launch/node.launch.py
@@ -1,11 +1,25 @@
+import os
 from launch import LaunchDescription
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
-
+from launch.substitutions import TextSubstitution, LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
+    
+        
     return LaunchDescription(
         [
+            # Arguments
+            DeclareLaunchArgument('config_filepath', default_value=[
+                TextSubstitution(text=os.path.join(
+                        get_package_share_directory('twig_hardware'),
+                        'config',
+                        'node.config.yaml'
+                )),
+            ]),
+            
             # Nodes
             ComposableNodeContainer(
                 name="twig_hardware_node_container",
@@ -17,10 +31,7 @@ def generate_launch_description():
                         name="twig_hardware_node",
                         package="twig_hardware",
                         plugin="twig_hardware::TwigHardwareNode",
-                        parameters=[{
-                            "push_rate": 50,
-                            "publish_rate": 50,
-                        }]
+                        parameters=[LaunchConfiguration('config_filepath')],
                     ),
                 ],
             )
