1bb5c89211eb5e69ffad018164d669286b84ca2e
nosknut <nosknut@gmail.com>
2024-05-01 13:36:09 +0200

    Adds correct topic configuration to teleop joy launchfile

--- src/twig_teleop/launch/joy.launch.py
+++ src/twig_teleop/launch/joy.launch.py
@@ -10,11 +10,18 @@ from launch.conditions import IfCondition, LaunchConfigurationEquals
 
 def generate_launch_description():
     return LaunchDescription([
+        # Arguments
+        DeclareLaunchArgument('joy_topic', default_value='/twig_joy'),
         DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
         DeclareLaunchArgument('deadzone', default_value="0.3"),
+        
+        # Nodes
         Node(
             package='joy',
             executable='joy_node',
+            remappings=[
+                ('/joy', LaunchConfiguration('joy_topic')),
+            ],
             parameters=[{
                 'dev': LaunchConfiguration('joy_dev'),
                 'deadzone': LaunchConfiguration('deadzone'),
